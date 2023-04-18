a0086b08bb35b28f1bca517ca3c35052aae44ad2
vinsevinalon <vinsevinalon@gmail.com>
2023-04-18 14:48:33 +0800

    Update: update components and integrate yelp API.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## ravenous
 
 A client-side React application, utilizing the Yelp API to search for local businesses.
+
+Node version: 16.20.0 LTS
+NPM version: 8.19.4
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,31 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+import Yelp from '../../util/Yelp';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
